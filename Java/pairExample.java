3f501227cb7b4508ac83d8de21cda0a455a256e1
Y0SDan <73656015+Y0SDan@users.noreply.github.com>
2023-01-19 13:25:38 -0600

    incios

---
+++ Java/pairExample.java
@@ -0,0 +1,14 @@
+class Pair(T, U){
+    private final T first;
+    private final U second;
+
+    public Pair(T firstm U second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst(){ return first; }
+    public U getSecond(){ return second; }
+}
+
+public class PairExample
